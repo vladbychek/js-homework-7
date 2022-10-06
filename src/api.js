063779217d57de2890f535d00b7e45c6f8347785
Vlad <vladbychek3@gmail.com>
2022-10-06 19:10:16 +0300

    working

---
+++ src/api.js
@@ -0,0 +1,20 @@
+async function getResponse() {
+    let response = await fetch('https://pokeapi.co/api/v2/pokemon/?offset=0&limit=20')
+    let content = await response.json()
+    
+
+    let list = document.querySelector('#all')
+
+    for(let key in content.results) {
+        list.innerHTML += `
+        <div id="pokemon">
+            <div id="pokemon_name">${content.results[key].name}</div>
+            <div id="pokemon_img">${content.results[key].url}</div>
+            <button id="pokemon_btn">Show more</button>
+        </div>
+        `
+    }
+}
+getResponse()
+
+
