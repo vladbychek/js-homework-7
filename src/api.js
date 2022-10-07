cd75d2fb0a47141190fe900494b271ffa697ba2b
Vlad <vladbychek3@gmail.com>
2022-10-07 23:50:43 +0300

    created pokemon images

--- src/api.js
+++ src/api.js
@@ -1,20 +1,36 @@
 async function getResponse() {
-    let response = await fetch('https://pokeapi.co/api/v2/pokemon/?offset=0&limit=20')
+    let url = 'https://pokeapi.co/api/v2/pokemon/?offset=0&limit=20'
+    let response = await fetch(url)
     let content = await response.json()
-    
+    content.results.forEach(function (pokemon){
+        getPokemon(pokemon);
+    })
 
-    let list = document.querySelector('#all')
 
-    for(let key in content.results) {
-        list.innerHTML += `
-        <div id="pokemon">
-            <div id="pokemon_name">${content.results[key].name}</div>
-            <div id="pokemon_img">${content.results[key].url}</div>
-            <button id="pokemon_btn">Show more</button>
-        </div>
-        `
-    }
 }
 getResponse()
+
+
+
+function getPokemon(pokemon){
+    let url = pokemon.url
+    fetch(url)
+        .then(response => response.json())
+        .then(function(pokeData){
+            let list = document.querySelector('#all')
+            for(let key in pokeData.forms) {
+                list.innerHTML += `
+                <div id="pokemon">
+                    <div id="pokemon_name">${pokeData.forms[key].name}</div>
+                    <img id="pokemon_img" src="${pokeData.sprites.other.dream_world.front_default}" alt="">
+                    <button id="pokemon_btn">Show more</button>
+                </div>
+                `
+            } 
+        })
+} 
+
+
+
 
 
