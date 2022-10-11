ef21092a5269ba68ea620055be7b8e4dc44f897f
Vlad <vladbychek3@gmail.com>
2022-10-11 15:13:23 +0300

    working

--- src/api.js
+++ src/api.js
@@ -1,5 +1,5 @@
 async function getResponse() {
-    let url = 'https://pokeapi.co/api/v2/pokemon/?offset=200&limit=20'
+    let url = 'https://pokeapi.co/api/v2/pokemon/?offset=0&limit=20'
     let response = await fetch(url)
     let content = await response.json()
     content.results.forEach(function (pokemon){
@@ -21,11 +21,41 @@ function getPokemon(pokemon){
             for(let key in arr) {
                 list.innerHTML += `
                 <div id="pokemon">
-                    <div id="pokemon_name">${arr[key].name}</div>
-                    <img id="pokemon_img" src="${pokeData.sprites.other.dream_world.front_default}" alt="">
-                    <button id="pokemon_btn">Show more</button>
-                </div>
-                `
+                    <div id="more_info">
+                        <div id="pokemon_name">${arr[key].name}</div>
+                        <img id="more_img1" src="${pokeData.sprites.back_default}" alt="">
+                        <img id="more_img2" src="${pokeData.sprites.front_default}" alt="">
+                        <div id="more_abil" class="abil">${pokeData.abilities[key].ability.name}</div>
+                        <button id="back_btn">Back to all</button>
+                    </div>
+                    <div id="static">
+                        <div id="pokemon_name">${arr[key].name}</div>
+                        <img id="pokemon_img" src="${pokeData.sprites.other.dream_world.front_default}" alt="">
+                        <button id="pokemon_btn">Show more</button>
+                    </div>
+                </div>`
             } 
         })
 } 
+
+function show() {
+    const show = document.getElementById('more_info')
+    const static = document.getElementById('static')
+    show.style.display = "block";
+    static.style.display = "none";
+}
+document.querySelector('#pokemon_btn').addEventListener('click', show)
+
+function hide() {
+    const show = document.getElementById('more_info')
+    const static = document.getElementById('static')
+    show.style.display = "none";
+    static.style.display = "block";
+}
+document.querySelector('#back_btn').addEventListener('click', hide)
+
+
+
+
+{/* <div id="more_abil">${pokeData.abilities[key].ability.name}</div> */}
+{/* <div id="more_abil">${pokeData.abilities[key].ability.name}</div> */}
