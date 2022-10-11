5f6c976dccf80719efdb156a13441a5696545e16
Vlad <vladbychek3@gmail.com>
2022-10-11 08:41:05 +0300

    created pokemon img and names

--- src/api.js
+++ src/api.js
@@ -1,12 +1,11 @@
-let number = 0
-
-async function getResponse(newNum) {
-    let url = 'https://pokeapi.co/api/v2/pokemon/?offset='+ newNum +'&limit=20'
+async function getResponse() {
+    let url = 'https://pokeapi.co/api/v2/pokemon/?offset=200&limit=20'
     let response = await fetch(url)
     let content = await response.json()
     content.results.forEach(function (pokemon){
         getPokemon(pokemon);
     })
+
 }
 
 getResponse()
@@ -16,11 +15,13 @@ function getPokemon(pokemon){
     fetch(url)
         .then(response => response.json())
         .then(function(pokeData){
+            let arr = []
+            arr.push(pokeData)
             let list = document.querySelector('#all')
-            for(let key in pokeData.species) {
+            for(let key in arr) {
                 list.innerHTML += `
                 <div id="pokemon">
-                    <div id="pokemon_name">${pokeData[key].name}</div>
+                    <div id="pokemon_name">${arr[key].name}</div>
                     <img id="pokemon_img" src="${pokeData.sprites.other.dream_world.front_default}" alt="">
                     <button id="pokemon_btn">Show more</button>
                 </div>
@@ -28,13 +29,3 @@ function getPokemon(pokemon){
             } 
         })
 } 
-
-function next(){
-    let newNum = number + 20
-    getResponse(newNum)
-    console.log(newNum)
-}
-
-document.querySelector('#next_btn').addEventListener('click', next)
-
-
