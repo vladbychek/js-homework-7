4c7c4acbabfbfd9165758602574921000c448897
Vlad <vladbychek3@gmail.com>
2022-10-13 17:52:14 +0300

    fixed pagination and poke-imges

--- api.js
+++ api.js
@@ -1,4 +1,4 @@
-baseUrl('https://pokeapi.co/api/v2/pokemon/?offset=0&limit=20')
+baseUrl('https://pokeapi.co/api/v2/pokemon/?offset=1120&limit=20')
 let nextPage
 let prevPage
 
@@ -10,6 +10,18 @@ async function baseUrl(mainLink) {
     content.results.forEach(function (pokemon){
         getPokemon(pokemon);
     })
+    if(!prevPage){
+        document.getElementById('prev_btn').setAttribute("disabled", true)
+    }
+    else{
+        document.getElementById('prev_btn').removeAttribute("disabled")
+    }
+    if(!nextPage){
+        document.getElementById('next_btn').setAttribute("disabled", true)
+    }
+    else{
+        document.getElementById('next_btn').removeAttribute("disabled")
+    }
 }
 
 let newPage = document.createElement('div')
@@ -22,8 +34,7 @@ function getPokemon(pokemon){
         .then(response => response.json())
         .then(function(pokeData){
             const arr = []
-            arr.push(pokeData)
-            console.log(arr)    
+            arr.push(pokeData)    
             for(let key in arr) {
                 new_page.innerHTML += `
                 <div id="pokemon">
@@ -32,19 +43,55 @@ function getPokemon(pokemon){
                         <img id="more_img1" src="${pokeData.sprites.back_default}" alt="back_pokemon">
                         <img id="more_img2" src="${pokeData.sprites.front_default}" alt="fron_pokemon">
                         <div id="more_abil" class="abil">${pokeData.abilities[0].ability.name}</div>
-                        <div id="more_abil">${pokeData.abilities[1].ability.name}</div>
-                        <div id="more_abil">${pokeData.abilities[2].ability.name}</div>
                         <button id="back_btn">Back to all</button>
                     </div>
                     <div id="static">
                         <div id="pokemon_name">${arr[key].name}</div>
-                        <img id="pokemon_img" src="${pokeData.sprites.other.dream_world.front_default}" alt="main_pokemon">
+                        <img id="pokemon_img" src="${pokeData.sprites.front_default}" alt="main_pokemon">
                         <button id="pokemon_btn">Show more</button>
                     </div>
                 </div>`
             }
         })
 } 
+
+
+function next() {
+    baseUrl(nextPage)
+    removeEl()
+    let newPage = document.createElement('div')
+    newPage.id = 'new_page'
+    all.append(newPage)
+
+}
+function prev() {
+    baseUrl(prevPage)
+    removeEl()
+    let newPage = document.createElement('div')
+    newPage.id = 'new_page'
+    all.append(newPage)
+
+}
+function removeEl() {
+    let del = document.querySelector('#new_page')
+    del.remove()
+};
+
+
+document.querySelector('#next_btn').addEventListener('click', next)
+document.querySelector('#prev_btn').addEventListener('click', prev)
+
+
+
+
+
+
+
+
+
+
+
+
 
 // function show() {
 //     const show = document.getElementById('more_info')
@@ -61,49 +108,3 @@ function getPokemon(pokemon){
 //     static.style.display = "block";
 // }
 // document.querySelector('#back_btn').addEventListener('click', hide)
-
-function next() {
-    baseUrl(nextPage)
-    removeEl()
-    let newPage = document.createElement('div')
-    newPage.id = 'new_page'
-    all.append(newPage)
-}
-function prev() {
-    baseUrl(prevPage)
-    removeEl()
-    let newPage = document.createElement('div')
-    newPage.id = 'new_page'
-    all.append(newPage)
-}
-function removeEl() {
-    let del = document.querySelector('#new_page')
-    del.remove()
-};
-document.querySelector('#next_btn').addEventListener('click', next)
-document.querySelector('#prev_btn').addEventListener('click', prev)
-
-document.querySelector('#next_btn').addEventListener('click', plusCount)
-document.querySelector('#prev_btn').addEventListener('click', minusCount)
-
-
-
-let b = document.querySelector('#prev_btn')
-
-let a = 0
-
-function plusCount() {
-    if(a >= 0){
-        a++
-    }
-    console.log(a)
-}
-function minusCount() {
-    if(a > 0){
-        a--
-    }
-    // }else{
-    //     b.setAttribute(disabled, true)
-    // }
-    console.log(a)
-}
