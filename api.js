5798d71357a3ace9420d559c0b9c20b2540a9692
Vlad <vladbychek3@gmail.com>
2022-10-14 13:00:10 +0300

    change device

--- api.js
+++ api.js
@@ -10,20 +10,45 @@ async function baseUrl(mainLink) {
     content.results.forEach(function (pokemon){
         getPokemon(pokemon);
     })
+    if(!prevPage){
+        document.getElementById('prev_btn').setAttribute("disabled", true)
+    }
+    else{
+        document.getElementById('prev_btn').removeAttribute("disabled")
+    }
+    if(!nextPage){
+        document.getElementById('next_btn').setAttribute("disabled", true)
+    }
+    else{
+        document.getElementById('next_btn').removeAttribute("disabled")
+    }
 }
+
+
+
 
 let newPage = document.createElement('div')
 newPage.id = 'new_page'
 all.append(newPage)
+
+
 
 function getPokemon(pokemon){
     let mainUrl = pokemon.url
     fetch(mainUrl)
         .then(response => response.json())
         .then(function(pokeData){
+            // console.log(pokeData.abilities)
+            const ul = document.createElement('ul');
+            let y = '<ul>';
+            for(a of pokeData.abilities){
+                y+= `<li>${a.ability.name}</li>`
+                
+            }
+            y += '</ul>'
+            // y.append(t1)
             const arr = []
-            arr.push(pokeData)
-            console.log(arr)    
+            arr.push(pokeData)    
             for(let key in arr) {
                 new_page.innerHTML += `
                 <div id="pokemon">
@@ -31,14 +56,12 @@ function getPokemon(pokemon){
                         <div id="pokemon_name">${arr[key].name}</div>
                         <img id="more_img1" src="${pokeData.sprites.back_default}" alt="back_pokemon">
                         <img id="more_img2" src="${pokeData.sprites.front_default}" alt="fron_pokemon">
-                        <div id="more_abil" class="abil">${pokeData.abilities[0].ability.name}</div>
-                        <div id="more_abil">${pokeData.abilities[1].ability.name}</div>
-                        <div id="more_abil">${pokeData.abilities[2].ability.name}</div>
+                        ${y}
                         <button id="back_btn">Back to all</button>
                     </div>
                     <div id="static">
                         <div id="pokemon_name">${arr[key].name}</div>
-                        <img id="pokemon_img" src="${pokeData.sprites.other.dream_world.front_default}" alt="main_pokemon">
+                        <img id="pokemon_img" src="${pokeData.sprites.front_default}" alt="main_pokemon">
                         <button id="pokemon_btn">Show more</button>
                     </div>
                 </div>`
@@ -46,21 +69,6 @@ function getPokemon(pokemon){
         })
 } 
 
-// function show() {
-//     const show = document.getElementById('more_info')
-//     const static = document.getElementById('static')
-//     show.style.display = "block";
-//     static.style.display = "none";
-// }
-// document.querySelector('#pokemon_btn').addEventListener('click', show)
-
-// function hide() {
-//     const show = document.getElementById('more_info')
-//     const static = document.getElementById('static')
-//     show.style.display = "none";
-//     static.style.display = "block";
-// }
-// document.querySelector('#back_btn').addEventListener('click', hide)
 
 function next() {
     baseUrl(nextPage)
@@ -68,6 +76,7 @@ function next() {
     let newPage = document.createElement('div')
     newPage.id = 'new_page'
     all.append(newPage)
+
 }
 function prev() {
     baseUrl(prevPage)
@@ -75,35 +84,47 @@ function prev() {
     let newPage = document.createElement('div')
     newPage.id = 'new_page'
     all.append(newPage)
+
 }
 function removeEl() {
     let del = document.querySelector('#new_page')
     del.remove()
 };
+
+
 document.querySelector('#next_btn').addEventListener('click', next)
 document.querySelector('#prev_btn').addEventListener('click', prev)
 
-document.querySelector('#next_btn').addEventListener('click', plusCount)
-document.querySelector('#prev_btn').addEventListener('click', minusCount)
 
 
 
-let b = document.querySelector('#prev_btn')
 
-let a = 0
 
-function plusCount() {
-    if(a >= 0){
-        a++
-    }
-    console.log(a)
-}
-function minusCount() {
-    if(a > 0){
-        a--
-    }
-    // }else{
-    //     b.setAttribute(disabled, true)
-    // }
-    console.log(a)
-}
+
+
+
+
+// // function show() {
+// //     const show = document.getElementById('more_info')
+// //     const static = document.getElementById('static')
+// //     show.style.display = "block";
+// //     static.style.display = "none";
+// // }
+// // document.querySelector('#pokemon_btn').addEventListener('click', show)
+
+// // function hide() {
+// //     const show = document.getElementById('more_info')
+// //     const static = document.getElementById('static')
+// //     show.style.display = "none";
+// //     static.style.display = "block";
+// // }
+// // document.querySelector('#back_btn').addEventListener('click', hide)
+
+
+
+// let arr = [5,6,1]
+// console.log(arr)
+
+// for(a in arr){
+//     console.log(arr[a])
+// }
