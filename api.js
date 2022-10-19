3928f82ffd4c9f1262f889477b1ac42b3adadb46
Vlad <vladbychek3@gmail.com>
2022-10-19 13:22:28 +0300

    working

--- api.js
+++ api.js
@@ -1,110 +1,94 @@
-baseUrl('https://pokeapi.co/api/v2/pokemon/?offset=1120&limit=20')
+baseUrl('https://pokeapi.co/api/v2/pokemon/?offset=0&limit=20')
 let nextPage
 let prevPage
 
 async function baseUrl(mainLink) {
-    let mainUrl = await fetch(mainLink)
-    let content = await mainUrl.json()
+    const mainUrl = await fetch(mainLink)
+    const content = await mainUrl.json()
     nextPage = content.next
     prevPage = content.previous
     content.results.forEach(function (pokemon){
         getPokemon(pokemon);
-    })
-    if(!prevPage){
-        document.getElementById('prev_btn').setAttribute("disabled", true)
-    }
-    else{
-        document.getElementById('prev_btn').removeAttribute("disabled")
-    }
-    if(!nextPage){
-        document.getElementById('next_btn').setAttribute("disabled", true)
-    }
-    else{
-        document.getElementById('next_btn').removeAttribute("disabled")
-    }
+    });
+    !prevPage ? document.querySelector('.prev_btn').style.display = "none" : document.querySelector('.prev_btn').style.display = "block"
+    !nextPage ? document.querySelector('.next_btn').style.display = "none" : document.querySelector('.next_btn').style.display = "block"
 }
 
-let newPage = document.createElement('div')
+const newPage = document.createElement('div')
 newPage.id = 'new_page'
 all.append(newPage)
 
 function getPokemon(pokemon){
-    let mainUrl = pokemon.url
+    const mainUrl = pokemon.url
     fetch(mainUrl)
         .then(response => response.json())
         .then(function(pokeData){
+            let ul = document.createElement('ul');
+            let abils = '<ul>';
+            for(a of pokeData.abilities){
+                abils += `<li>${a.ability.name}</li>`
+            }
+            abils += '</ul>'
             const arr = []
             arr.push(pokeData)    
+            console.log(pokeData)
             for(let key in arr) {
-                new_page.innerHTML += `
-                <div id="pokemon">
-                    <div id="more_info">
-                        <div id="pokemon_name">${arr[key].name}</div>
-                        <img id="more_img1" src="${pokeData.sprites.back_default}" alt="back_pokemon">
-                        <img id="more_img2" src="${pokeData.sprites.front_default}" alt="fron_pokemon">
-                        <div id="more_abil" class="abil">${pokeData.abilities[0].ability.name}</div>
-                        <button id="back_btn">Back to all</button>
+                new_page.innerHTML +=
+                 `<div class="pokemon">
+                    <div class="more_info">
+                        <div class="more_pokemon_name">${arr[key].name}</div>
+                        <img class="more_img1" src="${pokeData.sprites.back_default}" alt="back_pokemon">
+                        <img class="more_img2" src="${pokeData.sprites.front_default}" alt="front_pokemon">
+                        ${abils}
+                        <button class="back_btn">Back</button>
                     </div>
-                    <div id="static">
-                        <div id="pokemon_name">${arr[key].name}</div>
-                        <img id="pokemon_img" src="${pokeData.sprites.front_default}" alt="main_pokemon">
-                        <button id="pokemon_btn">Show more</button>
+                    <div class="static">
+                        <div class="pokemon_name">${arr[key].name}</div>
+                        <img class="pokemon_img" src="${pokeData.sprites.front_default}" alt="main_pokemon">
+                        <button class="pokemon_btn">Show more</button>
                     </div>
                 </div>`
             }
+            const wrappers = document.querySelectorAll('.pokemon');
+
+
+            [].forEach.call(wrappers,function(el){
+                const more_info = el.querySelector('.more_info')
+                const static_info = el.querySelector('.static')
+                static_info.querySelector('.pokemon_btn').addEventListener('click', () => {
+                    static_info.style.display = "none"
+                    more_info.style.display = "block"                   
+                })
+            });
+            [].forEach.call(wrappers,function(el){
+                const more_info = el.querySelector('.more_info')
+                const static_info = el.querySelector('.static')
+                more_info.querySelector('.back_btn').addEventListener('click', () => {
+                    static_info.style.display = "block"
+                    more_info.style.display = "none"                   
+                })
+            });
+
         })
 } 
 
-
-function next() {
-    baseUrl(nextPage)
-    removeEl()
-    let newPage = document.createElement('div')
-    newPage.id = 'new_page'
-    all.append(newPage)
-
-}
-function prev() {
-    baseUrl(prevPage)
-    removeEl()
-    let newPage = document.createElement('div')
-    newPage.id = 'new_page'
-    all.append(newPage)
-
-}
-function removeEl() {
-    let del = document.querySelector('#new_page')
-    del.remove()
+function removePage() {
+    const pageToDelete = document.querySelector('#new_page')
+    pageToDelete.remove()
 };
 
+document.querySelector('.next_btn').addEventListener('click', () => {
+    baseUrl(nextPage)
+    removePage()
+    const newPage = document.createElement('div')
+    newPage.id = 'new_page'
+    all.append(newPage)
+})
 
-document.querySelector('#next_btn').addEventListener('click', next)
-document.querySelector('#prev_btn').addEventListener('click', prev)
-
-
-
-
-
-
-
-
-
-
-
-
-
-// function show() {
-//     const show = document.getElementById('more_info')
-//     const static = document.getElementById('static')
-//     show.style.display = "block";
-//     static.style.display = "none";
-// }
-// document.querySelector('#pokemon_btn').addEventListener('click', show)
-
-// function hide() {
-//     const show = document.getElementById('more_info')
-//     const static = document.getElementById('static')
-//     show.style.display = "none";
-//     static.style.display = "block";
-// }
-// document.querySelector('#back_btn').addEventListener('click', hide)
+document.querySelector('.prev_btn').addEventListener('click', () => {
+    baseUrl(prevPage)
+    removePage()
+    const newPage = document.createElement('div')
+    newPage.id = 'new_page'
+    all.append(newPage)
+})
