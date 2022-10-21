8791852273b547e854424dab26c3a861062cb08f
Vlad <vladbychek3@gmail.com>
2022-10-21 12:30:09 +0300

    finished

--- api.js
+++ api.js
@@ -23,7 +23,8 @@ function getPokemon(pokemon){
     fetch(mainUrl)
         .then(response => response.json())
         .then(function(pokeData){
-            let ul = document.createElement('ul');
+            let ull = document.createElement('ul');
+            ull.className = 'abil_list'
             let abils = '<ul>';
             for(a of pokeData.abilities){
                 abils += `<li>${a.ability.name}</li>`
@@ -31,24 +32,45 @@ function getPokemon(pokemon){
             abils += '</ul>'
             const arr = []
             arr.push(pokeData)    
-            console.log(pokeData)
             for(let key in arr) {
+                console.log(pokeData.stats)
+                let img1=pokeData.sprites.front_default
+                let img2 = pokeData.sprites.back_default
+                if(!img2){
+                    img2="Q90w02NEjWg.jpg"
+                }
+                if(!img1){
+                    img1="Q90w02NEjWg.jpg"
+                }
                 new_page.innerHTML +=
                  `<div class="pokemon">
                     <div class="more_info">
                         <div class="more_pokemon_name">${arr[key].name}</div>
-                        <img class="more_img1" src="${pokeData.sprites.back_default}" alt="back_pokemon">
-                        <img class="more_img2" src="${pokeData.sprites.front_default}" alt="front_pokemon">
+                        <div class="more_img_wrapper">
+                            <img class="more_img1" src="${img2}" >
+                            <img class="more_img2" src="${img1}" >
+                        </div>
                         ${abils}
+                        <div class="more_stats">
+                            <div class="stat"><i class="fa-solid fa-heart hp"></i> ${pokeData.stats[0].base_stat}</div>
+                            <div class="stat"><i class="fa-solid fa-hand-back-fist dmg"></i> ${pokeData.stats[1].base_stat}</div>
+                            <div class="stat"><i class="fa-solid fa-shield def"></i> ${pokeData.stats[2].base_stat}</div>
+                            <div class="stat"><i class="fa-solid fa-bolt spec"></i> ${pokeData.stats[3].base_stat}</div>
+                            <div class="stat"><i class="fa-solid fa-person-running speed"></i> ${pokeData.stats[4].base_stat}</div>
+                        </div>
                         <button class="back_btn">Back</button>
                     </div>
                     <div class="static">
                         <div class="pokemon_name">${arr[key].name}</div>
-                        <img class="pokemon_img" src="${pokeData.sprites.front_default}" alt="main_pokemon">
+                        <div class="static_img_wrapper"><img class="pokemon_img" src="${img1}"></div>
                         <button class="pokemon_btn">Show more</button>
                     </div>
                 </div>`
             }
+
+
+
+
             const wrappers = document.querySelectorAll('.pokemon');
 
 
